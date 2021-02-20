61175b4d83789608df7f4edd2dfb2f5899159875
ZephyrChole <1905616103@qq.com>
2021-02-20 21:16:14 +0800

    adapt to bt panel daily run

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ from custom_record import CustomRecordDownloader
 # @software: PyCharm
 # @file: main.py
 # @time: 2/20/2021 1:00 PM
+
+import os
 from live_record import LiveRecordDownloader
 
 
 def main():
+    cur_path = '/home/pi/programs/bilibili_manager'
+    os.chdir(cur_path)
     crDownloader = CustomRecordDownloader(9035182, r'/home/pi/programs/bili',
                                           r'/media/pi/sda1/media/bilibili_record/3509872-有毒的小蘑菇酱-official/投稿视频')
     lrDownloader = LiveRecordDownloader('3509872', r'/home/pi/programs/bili',
