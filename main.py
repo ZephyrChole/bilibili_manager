64045e1755e601af2b15d5d11bf7ea633d7052d7
ZephyrChole <1905616103@qq.com>
2021-02-21 22:19:12 +0800

    modified parameter and path

--- main.py
+++ main.py
@@ -12,17 +12,6 @@ from live_record import LiveRecordDownloader
 
 
 def main():
-    cur_path = '/home/pi/programs/bilibili_manager'
-    os.chdir(cur_path)
-    logger = logging.getLogger('LR')
-    logger.setLevel(logging.INFO)
-    console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.INFO)
-    logger.addHandler(console_handler)
-    LRDownloader = LiveRecordDownloader('3509872', r'E:\playground\from github\bili',
-                                        r'I:\media\bilibili_record\3509872-有毒的小蘑菇酱\test_record', logger)
-    LRDownloader.main()
-
     logger = logging.getLogger('CR')
     logger.setLevel(logging.INFO)
     console_handler = logging.StreamHandler()
@@ -31,6 +20,18 @@ def main():
     crDownloader = CustomRecordDownloader(9035182, r'/media/pi/sda1/media/programs/bili',
                                           r'/media/pi/sda1/media/bilibili_record/3509872-有毒的小蘑菇酱-official/投稿视频', logger)
     crDownloader.main()
+
+    cur_path = '/home/pi/programs/bilibili_manager'
+    os.chdir(cur_path)
+    logger = logging.getLogger('LR')
+    logger.setLevel(logging.INFO)
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+    logger.addHandler(console_handler)
+    LRDownloader = LiveRecordDownloader('3509872', r'/media/pi/sda1/media/programs/bili',
+                                        r'/media/pi/sda1/media/bilibili_record/3509872-有毒的小蘑菇酱-official/投稿视频', logger)
+    LRDownloader.main()
+
 
 
 if __name__ == '__main__':
